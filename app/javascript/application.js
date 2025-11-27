566193b9b1fa9371d3b83d7974613982f69618f9
ccNatang <ncole1128@gmail.com>
2025-11-26 21:15:20 -0600

    feat: implementing React to project

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,21 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "controllers"
+import "./controllers"
+import React from "react";
+import { createRoot } from "react-dom/client";
+import HelloWorld from "./components/HelloWorld";
+
+console.log("1. Application.js is running!");
+
+document.addEventListener("turbo:load", () => {
+  console.log("2. Turbo load event fired!");
+  const rootElement = document.getElementById("react-root");
+  
+  if (rootElement) {
+    console.log("3. Found the root element! Mounting React...");
+    const root = createRoot(rootElement);
+    root.render(<HelloWorld />);
+  } else {
+    console.log("3. ERROR: Could not find element with id 'react-root'");
+  }
+});
---
+++ app/javascript/components/HelloWorld.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HelloWorld = () => {
+  return (
+    <div className="bg-blue-100 border-l-4 border-blue-500 text-blue-700 p-4 my-4 rounded shadow-md">
+      <p className="font-bold">Success!</p>
+      <p>React is running AND Tailwind CSS is applying styles correctly.</p>
+    </div>
+  );
+};
+
+export default HelloWorld;
